75019ea1e1f5fc205ab2cf2ec3b7ec0ba4bafca6
amora <39012331+amora2860@users.noreply.github.com>
2024-08-30 13:07:23 -0400

    Update Main.java

--- Assignment1/src/main/java/Main.java
+++ Assignment1/src/main/java/Main.java
@@ -61,7 +61,8 @@ public class Main {
         button1Create.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
 
-                int bstData[] = { 1, 2, 3, 4, 5, 6, 7 };
+                //int bstData[] = { 1, 2, 3, 4, 5, 6, 7 };
+                int bstData[] = { 4, 2, 6, 1, 3, 5, 7 };
 
                 // Checking the binary search tree doesn't already exist.
                 if (binaryTree.root == null) {
