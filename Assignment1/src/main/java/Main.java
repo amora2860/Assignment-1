673315283afda1fee689c10026a27c6c7e71076b
amora <39012331+amora2860@users.noreply.github.com>
2024-09-02 08:30:37 -0400

    Removed Java swift interface.

--- Assignment1/src/main/java/Main.java
+++ Assignment1/src/main/java/Main.java
@@ -6,6 +6,7 @@
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -15,187 +16,162 @@ public class Main {
     public static ArrayList<Integer> myList = new ArrayList<Integer>();
 
     public static void main(String[] args) {
-        // Creating the instance with labeling for the frame and each button.
-        JFrame myFrame = new JFrame("Assignment #1");
-        JButton button1Create = new JButton("1: Create A Binary Search Tree");
-        JButton button2Add = new JButton("2: Add A Node");
-        JButton button3Delete = new JButton("3: Delete A Node");
-        JButton button4Inorder = new JButton("4: Print Nodes By InOrder");
-        JButton button5Preorder = new JButton("5: Print Nodes By PreOrder");
-        JButton button6Postorder = new JButton("6: Print Nodes By PostOrder");
-        JButton button7Exit = new JButton("7: Exit Program");
-        JLabel labelChoose = new JLabel("Please select a button.");
 
-        // Specifying the size of the frame.
-        myFrame.setSize(500, 500);
+        // Must ensure the menu always comes up after option 1 or 2 is selected
+        while (true) {
 
-        // Ensuring that absolute positioning is used for the buttons.
-        myFrame.setLayout(null);
-        myFrame.setVisible(true);
+            System.out.println("Please enter a number from 1 - 7 from the menu below.");
+            System.out.println();
+            System.out.println("1: Create A Binary Search Tree");
+            System.out.println("2: Add A Node");
+            System.out.println("3: Delete A Node");
+            System.out.println("4: Print Nodes By InOrder");
+            System.out.println("5: Print Nodes By PreOrder");
+            System.out.println("6: Print Nodes By PostOrder");
+            System.out.println("7: Exit Program");
 
-        // Setting the absolute positions for all of the buttons.
-        button1Create.setBounds(100, 50, 250, 40);
-        button2Add.setBounds(100, 100, 170, 40);
-        button3Delete.setBounds(100, 150, 170, 40);
-        button4Inorder.setBounds(100, 200, 240, 40);
-        button5Preorder.setBounds(100, 250, 240, 40);
-        button6Postorder.setBounds(100, 300, 240, 40);
-        button7Exit.setBounds(100, 350, 170, 40);
-        labelChoose.setBounds(100, 10, 200, 40);
+            try {
 
-        // Adding each button to the frame.
-        myFrame.add(button1Create);
-        myFrame.add(button2Add);
-        myFrame.add(button3Delete);
-        myFrame.add(button4Inorder);
-        myFrame.add(button5Preorder);
-        myFrame.add(button6Postorder);
-        myFrame.add(button7Exit);
-        myFrame.add(labelChoose);
+                Scanner scanner = new Scanner(System.in);
+                int choice = scanner.nextInt();
 
-        // Close program if X in the top right corner of the frame is clicked on.
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                // Display the text of state names
+                if (choice == 1) {
+                    int bstData[] = { 1, 2, 3, 4, 5, 6, 7 };
+                    //int bstData[] = { 4, 2, 6, 1, 3, 5, 7 };
 
-        // This button creates the initial binary tree based off of the values in the data[] array.
-        // An action listener is needed to ensure that when the button is clicked the code will be executed.
-        button1Create.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-
-                //int bstData[] = { 1, 2, 3, 4, 5, 6, 7 };
-                int bstData[] = { 4, 2, 6, 1, 3, 5, 7 };
-
-                // Checking the binary search tree doesn't already exist.
-                if (binaryTree.root == null) {
-                    // Adding each value of the array into the binary tree.
-                    for (int i = 0; i < bstData.length; i++) {
-                        binaryTree.insert(bstData[i]);
+                    // Checking the binary search tree doesn't already exist.
+                    if (binaryTree.root == null) {
+                        // Adding each value of the array into the binary tree.
+                        for (int i = 0; i < bstData.length; i++) {
+                            binaryTree.insert(bstData[i]);
+                        }
+                        // The user needs to know that the binary tree has been created. So they do not click the button again.
+                        System.out.println("A binary search tree has been created.");
                     }
-                    // The user needs to know that the binary tree has been created. So they do not click the button again.
-                    JOptionPane.showMessageDialog(null, "A binary search tree has been created.");
-                }
-                else {
-                    // If the user clicks the button after the binary tree is created. They will be told that the binary tree has already been created.
-                    JOptionPane.showMessageDialog(null, "A binary search tree already exists.");
-                }
-            }
-        });
-        // This button adds a node to the binary tree.
-        button2Add.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // Ensuring a binary tree is already created.
-                if (binaryTree.root != null) {
-                    // When the user closes the text box by clicking cancel or the X. The key will be null.
-                    // We can only check if the value is null if the input is turned into a string.
-                    String keyValue = JOptionPane.showInputDialog("Enter an integer that you would like to add? ");
-                    // As long as the user has entered something then the value can be executed on.
-                    if (keyValue != null) {
-                        // If the value is anything other than an integer the catch block will inform the user of the error.
-                        try {
-                            // Turn the string into an integer.
-                            int keyInt = Integer.parseInt(keyValue);
-                            // Add the integer into the binary search tree.
-                            binaryTree.insert(keyInt);
-
-                        }
-                        catch (Exception ect) {
-                            // The user must be informed that they entered the wrong type.
-                            JOptionPane.showMessageDialog(null, "The entered value must be an integer.");
-                        }
+                    else {
+                        // If the user clicks the button after the binary tree is created. They will be told that the binary tree has already been created.
+                        System.out.println("A binary search tree already exists.");
                     }
                 }
-                else {
-                    // The user needs to know if the binary search tree does not exist.
-                    JOptionPane.showMessageDialog(null, "You must create the binary search tree first.");
-                }
-            }
-        });
-        // This button deletes a node from the binary tree.
-        button3Delete.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
+                // Search for a pattern in the text
+                if (choice == 2) {
+                    // Ensuring a binary tree is already created.
+                    if (binaryTree.root != null) {
+                        // When the user closes the text box by clicking cancel or the X. The key will be null.
+                        // We can only check if the value is null if the input is turned into a string.
+                        System.out.println("Enter an integer that you would like to add? ");
+                        scanner.nextLine(); // Consume newline
+                        String keyValue = scanner.nextLine();
+                        // As long as the user has entered something then the value can be executed on.
+                        if (keyValue != null) {
+                            // If the value is anything other than an integer the catch block will inform the user of the error.
+                            try {
+                                // Turn the string into an integer.
+                                int keyInt = Integer.parseInt(keyValue);
+                                // Add the integer into the binary search tree.
+                                binaryTree.insert(keyInt);
 
-                if (binaryTree.root != null) {
-
-                    String keyValue = JOptionPane.showInputDialog("Enter a number to be found: ");
-
-                    if (keyValue != null) {
-                        try {
-                            int keyInt = Integer.parseInt(keyValue);
-                            // Searching that the value exists in the binary search tree.
-                            if (binaryTree.find(keyInt) != null) {
-                                // If the value is found then it will be deleted.
-                                binaryTree.deleteThisNode(binaryTree.root, keyInt);
                             }
-                            else {
-                                JOptionPane.showMessageDialog(null, "Node not found.");
+                            catch (Exception ect) {
+                                // The user must be informed that they entered the wrong type.
+                                System.out.println("The entered value must be an integer.");
                             }
                         }
-                        catch (Exception ect) {
-                            // The user must be informed that they entered the wrong type.
-                            JOptionPane.showMessageDialog(null, "The entered value must be an integer.");
-                        }
+                    }
+                    else {
+                        // The user needs to know if the binary search tree does not exist.
+                        System.out.println("You must create the binary search tree first.");
                     }
                 }
-                else {
-                    // The user needs to know if the binary search tree does not exist
-                    JOptionPane.showMessageDialog(null, "You must create the binary search tree first.");
+
+                if (choice == 3) {
+                    if (binaryTree.root != null) {
+
+                        System.out.println("Enter a number to be found: ");
+                        scanner.nextLine(); // Consume newline
+                        String keyValue = scanner.nextLine();
+
+                        if (keyValue != null) {
+                            try {
+                                int keyInt = Integer.parseInt(keyValue);
+                                // Searching that the value exists in the binary search tree.
+                                if (binaryTree.find(keyInt) != null) {
+                                    // If the value is found then it will be deleted.
+                                    binaryTree.deleteThisNode(binaryTree.root, keyInt);
+                                }
+                                else {
+                                    System.out.println("Node not found.");
+                                }
+                            }
+                            catch (Exception ect) {
+                                // The user must be informed that they entered the wrong type.
+                                System.out.println("The entered value must be an integer.");
+                            }
+                        }
+                    }
+                    else {
+                        // The user needs to know if the binary search tree does not exist
+                        System.out.println("You must create the binary search tree first.");
+                    }
                 }
+
+                if (choice == 4) {
+                    // The array myList needs to be empty before we put anything into it.
+                    myList.clear();
+                    if (binaryTree.root != null) {
+                        // The PrintInOrder method is called and myList is updated and returned.
+                        binaryTree.printInOrder(binaryTree.root, myList);
+                        System.out.println("Nodes printed in order: " + myList);
+                    }
+                    else {
+                        System.out.println("You must create the binary search tree first.");
+                    }
+                }
+
+                if (choice == 5) {
+                    myList.clear();
+                    if (binaryTree.root != null) {
+                        // The printPreOrder method is called and myList is updated and returned.
+                        binaryTree.printPreOrder(binaryTree.root, myList);
+                        System.out.println( "Nodes printed in preorder: " + myList);
+                    }
+                    else {
+                        System.out.println("You must create the binary search tree first.");
+                    }
+                }
+
+                if (choice == 6) {
+                    myList.clear();
+
+                    if (binaryTree.root != null) {
+                        // The printPostOrder method is called and myList is updated and returned.
+                        binaryTree.printPostOrder(binaryTree.root, myList);
+
+                        System.out.println("Nodes printed in post order: " + myList);
+
+                    }
+                    else {
+                        System.out.println("You must create the binary search tree first.");
+                    }
+                }
+                // Exit the program
+                if (choice == 7) {
+                    System.out.println("Ending Program.");
+                    System.exit(0);
+                }
+
+                // Handle invalid menu selection
+                if (choice > 7 || choice < 1) {
+                    System.out.println("Please only enter a number from 1 through 7.");
+                }
+                // Handle any characters entered other than whole numbers
+            } catch (Exception e) {
+                System.out.println("Only whole numbers are accepted for menu selection.");
             }
-        });
-        // This button prints the binary tree inorder.
-        button4Inorder.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // The array myList needs to be empty before we put anything into it.
-                myList.clear();
-                if (binaryTree.root != null) {
-                    // The PrintInOrder method is called and myList is updated and returned.
-                    binaryTree.printInOrder(binaryTree.root, myList);
-                    JOptionPane.showMessageDialog(null, "Nodes printed in order: " + myList);
-                }
-                else {
-                    JOptionPane.showMessageDialog(null, "You must create the binary search tree first.");
-                }
-            }
-        });
-        // This button prints the binary tree in preorder.
-        button5Preorder.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                myList.clear();
-                if (binaryTree.root != null) {
-                    // The printPreOrder method is called and myList is updated and returned.
-                    binaryTree.printPreOrder(binaryTree.root, myList);
-                    JOptionPane.showMessageDialog(null, "Nodes printed in preorder: " + myList);
-                }
-                else {
-                    JOptionPane.showMessageDialog(null, "You must create the binary search tree first.");
-                }
-            }
-        });
-        // This button prints the binary tree in post order.
-        button6Postorder.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-
-                myList.clear();
-
-                if (binaryTree.root != null) {
-                    // The printPostOrder method is called and myList is updated and returned.
-                    binaryTree.printPostOrder(binaryTree.root, myList);
-
-                    JOptionPane.showMessageDialog(null, "Nodes printed in post order: " + myList);
-
-                }
-                else {
-                    JOptionPane.showMessageDialog(null, "You must create the binary search tree first.");
-                }
-
-            }
-        });
-        // This button exits the program when clicked.
-        button7Exit.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
-
+        }
     }
 
 }
+
+
