e20712de428fa271b70eb16d4155434c39a0aa2f
amora <39012331+amora2860@users.noreply.github.com>
2024-09-02 14:24:12 -0400

    Mostly revised based on feedbacked

--- Assignment1/src/main/java/BinarySearchTree.java
+++ Assignment1/src/main/java/BinarySearchTree.java
@@ -1,59 +1,234 @@
 import java.util.ArrayList;
 
 public class BinarySearchTree {
-    // Initializing the binary search tree by setting the root to point at no other
-    // nodes.
+    // Initializing the binary search tree by setting the root to point at no other nodes.
     public BinarySearchTree() {
         root = null;
     }
 
-    // This defines what a node contains and what the names are for the connecting
-    // nodes.
+    // This defines what a node contains and what the names are for the connecting nodes.
     class Node {
         int data;
         Node leftChild;
         Node rightChild;
+        int height;
 
         // Initial creation of the node and setting values to null.
         Node(int data) {
             this.data = data;
             leftChild = rightChild = null;
+            height = 1;
         }
     }
 
     // Initializing the root node.
     Node root;
+    //
+    static int getHeight(Node node) {
+        if (node == null)
+            return 0;
+        return node.height;
+    }
+
+
+    static Node rightRotation(Node node) {
+        Node n1 = node.leftChild;
+        Node n2 = n1.rightChild;
+
+        // Rotate nodes
+        n1.rightChild = node;
+        node.leftChild = n2;
+
+        //
+        node.height = 1 + Math.max(getHeight(node.leftChild),
+                getHeight(node.rightChild));
+        n1.height = 1 + Math.max(getHeight(n1.leftChild),
+                getHeight(n1.rightChild));
+
+        // Return updated root
+        return n1;
+    }
+
+    //
+    static Node leftRotation(Node node) {
+        Node n1 = node.rightChild;
+        Node n2 = n1.leftChild;
+
+        // Rotate nodes
+        n1.leftChild = node;
+        node.rightChild = n2;
+
+        //
+        node.height = 1 + Math.max(getHeight(node.leftChild),
+                getHeight(node.rightChild));
+        n1.height = 1 + Math.max(getHeight(n1.leftChild),
+                getHeight(n1.rightChild));
+
+        // Return updated root
+        return n1;
+    }
+
+
 
     // This method calls for the insertNode method. It saves the returned node to
     // the root.
-    public void insert(int key) {
-        root = insertNode(root, key);
+    public void insert(int data) {
+        root = insertNode(root, data);
     }
 
     // This method recursively navigates the binary search tree when it finds the
     // proper place to insert the node.
-    public Node insertNode(Node node, int key) {
+    public Node insertNode(Node node, int data) {
         // If the starting node is null then the node is created.
         if (node == null) {
-            node = new Node(key);
+            node = new Node(data);
             return node;
         }
         // If the key to be added is less than or equal to then go to the left child and
         // keep searching recursively.
-        if (key <= node.data) {
-            node.leftChild = insertNode(node.leftChild, key);
+        if (data <= node.data) {
+            node.leftChild = insertNode(node.leftChild, data);
 
         }
-        // If the key to be added is greater than the node then go to the right child
-        // and keep searching recursively.
-        else if (key > node.data) {
-            node.rightChild = insertNode(node.rightChild, key);
+        // If the key to be added is greater than the node then go to the right child and keep searching recursively.
+        else if (data > node.data) {
+            node.rightChild = insertNode(node.rightChild, data);
+
+        } else {
+            return node;
+        }
+
+        return balanceTree(node, data);
+
+    }
+
+    static int getBalance(Node node) {
+        if (node == null)
+            return 0;
+        return getHeight(node.leftChild) - getHeight(node.rightChild);
+    }
+
+    // need a function called balance tree
+    public Node balanceTree(Node node, int data){
+        // Update height of this ancestor node
+        node.height = 1 + Math.max(getHeight(node.leftChild),
+                getHeight(node.rightChild));
+
+        // Get the balance factor of this node
+        int balance = getBalance(node);
+
+
+
+        // Left Left Case
+        if (balance > 1 && data < node.leftChild.data)
+            return rightRotation(node);
+
+        // Right Right Case
+        if (balance < -1 && data > node.rightChild.data)
+            return leftRotation(node);
+
+        // Left Right Case
+        if (balance > 1 && data > node.leftChild.data) {
+            node.leftChild = leftRotation(node.leftChild);
+            return rightRotation(node);
+        }
+
+        // Right Left Case
+        if (balance < -1 && data < node.rightChild.data) {
+            node.rightChild = rightRotation(node.rightChild);
+            return leftRotation(node);
+        }
+
+        // Return the (unchanged) node pointer
+        return node;
+    }
+    // This method applies the root from the binary search tree to the findThisNode along with the value we are trying
+    // to find.
+    public Node find(int data) {
+
+        Node node = findThisNode(root, data);
+
+        return node;
+    }
+
+    public Node findThisNode(Node node, int data) {
+        // If the node is not found, return null so the user can be informed.
+        if (node == null) {
+            return null;
+        }
+
+        // If the key equals the node then return the node.
+        if (data == node.data) {
+
+            return node;
+        }
+        // If the key is less than the node's data, search the left child recursively.
+        else if (data <= node.data) {
+            return findThisNode(node.leftChild, data);
+        }
+        // If the key is greater than the node's data, search the right child
+        // recursively.
+        else if (data > node.data) {
+            return findThisNode(node.rightChild, data);
         }
         return node;
     }
 
+    // This method updates the root of the binary search tree.
+    // This is performed by calling deletedNode() and passing in the root compiled in deletedNode.
+    public void deleteThisNode(Node node, int data) {
+        root = deletedNode(node, data);
+    }
 
-    // Printing inorder is having the nodes values printed from smallest to largest. (ie 1,2,3.. etc.)
+    // This method recursively searches the binary search tree for the node to be deleted.
+    public Node deletedNode(Node node, int data) {
+
+        // If the key is greater than the root then go to the right child until the key is no longer greater than the root.
+        if (node.data < data) {
+            node.rightChild = deletedNode(node.rightChild, data);
+        }
+        // If the key is less than the root then go to the left child until the key is no longer less than the root.
+        else if (node.data > data) {
+            node.leftChild = deletedNode(node.leftChild, data);
+        }
+        // At this point the root.data equals the key.
+        else {
+
+            // If the root has no left child then return the root's right child so the node is removed.
+            if (node.leftChild == null) {
+                return node.rightChild;
+            }
+            // If the current node has no right child then return the left child so the node is removed.
+            if (node.rightChild == null) {
+                return node.leftChild;
+            }
+
+            // We are looking for the smallest value linked to the right child of the node to be deleted.
+            Node successor = findSuccessor(node.rightChild);
+
+            // Updating the root with the successor.
+            node.data = successor.data;
+
+            // The successor needs to be deleted from the right child of the root.
+            node.rightChild = deletedNode(node.rightChild, successor.data);
+
+            return node;
+        }
+
+        return balanceTree(node, data);
+
+    }
+
+    // Since the findSuccessor already provided the right child of the node to be deleted we only need to find the last left child linked.
+    public Node findSuccessor(Node node) {
+
+        while (node.leftChild != null) {
+            node = node.leftChild;
+        }
+        return node;
+    }
+
+    // Printing inorder is having the node's values printed from smallest to largest. (ie 1,2,3.. etc.)
     public ArrayList<Integer> printInOrder(Node node, ArrayList<Integer> list) {
         if (node != null) {
             // Navigate to the last node in the left subtree.
@@ -82,9 +257,9 @@ public class BinarySearchTree {
         return list;
     }
 
-    // Printing in post order means out the last node on the left subtree, then the node on the right subtree.
+    // Printing in post-order means out the last node on the left subtree, then the node on the right subtree.
     // The process involves trimming the leaves from left to right until you reach the root.
-    // The root is the last node.
+    // The root is the last node printed.
     public ArrayList<Integer> printPostOrder(Node node, ArrayList<Integer> list) {
         if (node != null) {
             // Navigate to the last node in the left subtree.
@@ -98,97 +273,6 @@ public class BinarySearchTree {
         return list;
     }
 
-    // This method applies the root from the binary search tree to the findThisNode
-    // along with the value we are trying to find.
-    public Node find(int key) {
 
-        Node node = findThisNode(root, key);
-
-        return node;
-    }
-
-    public Node findThisNode(Node node, int key) {
-        // If the node is not found, return null so the user can be informed.
-        if (node == null) {
-            return null;
-        }
-
-        // If the key equals the node then return the node.
-        if (key == node.data) {
-
-            return node;
-        }
-        // If the key is less than the node's data, search the left child recursively.
-        else if (key <= node.data) {
-            return findThisNode(node.leftChild, key);
-        }
-        // If the key is greater than the node's data, search the right child
-        // recursively.
-        else if (key > node.data) {
-            return findThisNode(node.rightChild, key);
-        }
-        return node;
-    }
-
-    // This method updates the root of the binary search tree.
-    // This is performed by calling deletedNode() and passing in the root compiled
-    // in deletedNode.
-    public void deleteThisNode(Node node, int key) {
-        root = deletedNode(root, key);
-    }
-
-    // This method recursively searches the binary search tree for the node to be
-    // deleted.
-    public Node deletedNode(Node root, int key) {
-
-        // If the key is greater than the root then go to the right child until the key
-        // is no longer greater than the root.
-        if (root.data < key) {
-            root.rightChild = deletedNode(root.rightChild, key);
-        }
-        // If the key is less than the root then go to the left child until the key is
-        // no longer less than the root.
-        else if (root.data > key) {
-            root.leftChild = deletedNode(root.leftChild, key);
-        }
-        // At this point the root.data equals the key.
-        else {
-
-            // If the root has no left child then return the root's right child so the node
-            // is removed.
-            if (root.leftChild == null) {
-                return root.rightChild;
-            }
-            // If the current node has no right child then return the left child so the node
-            // is removed.
-            if (root.rightChild == null) {
-                return root.leftChild;
-            }
-
-            // We are looking for the smallest value that is linked to the right child of
-            // the node to be deleted.
-            Node successor = findSuccessor(root.rightChild);
-
-            // Updating the root with the successor.
-            root.data = successor.data;
-
-            // The successor needs to be deleted from the right child of the root.
-            root.rightChild = deletedNode(root.rightChild, successor.data);
-
-            return root;
-        }
-        return root;
-
-    }
-
-    // Since the findSuccessor already provided the right child of the node to be
-    // deleted we only need to find the last left child linked.
-    public Node findSuccessor(Node root) {
-
-        while (root.leftChild != null) {
-            root = root.leftChild;
-        }
-        return root;
-    }
 
 }
--- Assignment1/src/main/java/Main.java
+++ Assignment1/src/main/java/Main.java
@@ -1,23 +1,17 @@
 
-// This program provides a user interface that allows the user to create a binary search tree.
-// The user can insert and delete nodes on the tree.
-// Lastly the user can print out the tree in several different orders. Inorder, Preorder and Postorder.
-
-import java.awt.event.*;
-import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    // The binaryTree is initialized here so it can be used in all methods and that only one binary tree is created.
+    // Using one binary search tree in this program
     public static BinarySearchTree binaryTree = new BinarySearchTree();
-    // Having a single shareable array list. Makes printing the binary tree to the users more simple.
+    // Using one array list for all printing operations
     public static ArrayList<Integer> myList = new ArrayList<Integer>();
 
     public static void main(String[] args) {
 
-        // Must ensure the menu always comes up after option 1 or 2 is selected
+        // Must ensure the menu always comes up regardless of operation selected.
         while (true) {
 
             System.out.println("Please enter a number from 1 - 7 from the menu below.");
@@ -31,125 +25,79 @@ public class Main {
             System.out.println("7: Exit Program");
 
             try {
-
+                // Storing user's input
                 Scanner scanner = new Scanner(System.in);
                 int choice = scanner.nextInt();
 
-                // Display the text of state names
+                // Create the binary search tree
                 if (choice == 1) {
-                    int bstData[] = { 1, 2, 3, 4, 5, 6, 7 };
-                    //int bstData[] = { 4, 2, 6, 1, 3, 5, 7 };
+                    int[] bstData = { 1, 2, 3, 4, 5, 6, 7 };
 
-                    // Checking the binary search tree doesn't already exist.
+                    // Checking the binary search tree doesn't yet exist.
                     if (binaryTree.root == null) {
                         // Adding each value of the array into the binary tree.
-                        for (int i = 0; i < bstData.length; i++) {
-                            binaryTree.insert(bstData[i]);
+                        for (int bstDatum : bstData) {
+                            binaryTree.insert(bstDatum);
                         }
-                        // The user needs to know that the binary tree has been created. So they do not click the button again.
+                        // The user needs to know that the binary tree has been created
                         System.out.println("A binary search tree has been created.");
                     }
                     else {
-                        // If the user clicks the button after the binary tree is created. They will be told that the binary tree has already been created.
-                        System.out.println("A binary search tree already exists.");
+                        // The user needs to be told that the binary tree has already been created.
+                        System.out.println("The binary search tree has already been created.");
                     }
                 }
-                // Search for a pattern in the text
-                if (choice == 2) {
-                    // Ensuring a binary tree is already created.
-                    if (binaryTree.root != null) {
-                        // When the user closes the text box by clicking cancel or the X. The key will be null.
-                        // We can only check if the value is null if the input is turned into a string.
-                        System.out.println("Enter an integer that you would like to add? ");
-                        scanner.nextLine(); // Consume newline
-                        String keyValue = scanner.nextLine();
-                        // As long as the user has entered something then the value can be executed on.
-                        if (keyValue != null) {
-                            // If the value is anything other than an integer the catch block will inform the user of the error.
-                            try {
-                                // Turn the string into an integer.
-                                int keyInt = Integer.parseInt(keyValue);
-                                // Add the integer into the binary search tree.
-                                binaryTree.insert(keyInt);
 
-                            }
-                            catch (Exception ect) {
-                                // The user must be informed that they entered the wrong type.
-                                System.out.println("The entered value must be an integer.");
-                            }
+                if (choice <= 6 && choice >= 2) {
+                    // Check if the BST already exists before moving on.
+                    if (binaryTree.root != null) {
+                        // The array myList needs to be empty before we put anything into it.
+                        myList.clear();
+                        // Add a node to the BST
+                        if (choice == 2) {
+                                System.out.println("Enter an integer that you would like to add? ");
+                                scanner.nextLine(); // Consume newline
+                                int keyInt = scanner.nextInt();
+
+
+                                
+                                    //System.out.println();
+                                   // Add the integer into the binary search tree.
+                                   binaryTree.insert(keyInt);
+
                         }
-                    }
-                    else {
-                        // The user needs to know if the binary search tree does not exist.
-                        System.out.println("You must create the binary search tree first.");
-                    }
-                }
-
-                if (choice == 3) {
-                    if (binaryTree.root != null) {
-
-                        System.out.println("Enter a number to be found: ");
-                        scanner.nextLine(); // Consume newline
-                        String keyValue = scanner.nextLine();
-
-                        if (keyValue != null) {
-                            try {
-                                int keyInt = Integer.parseInt(keyValue);
+                        // Delete a node in the binary search tree
+                        if (choice == 3) {
+                                System.out.println("Enter the number of the node you would like to delete. ");
+                                scanner.nextLine(); // Consume newline
+                                int keyInt = scanner.nextInt();
                                 // Searching that the value exists in the binary search tree.
                                 if (binaryTree.find(keyInt) != null) {
-                                    // If the value is found then it will be deleted.
+                                    // If the value is found, then it will be deleted.
                                     binaryTree.deleteThisNode(binaryTree.root, keyInt);
                                 }
                                 else {
                                     System.out.println("Node not found.");
                                 }
-                            }
-                            catch (Exception ect) {
-                                // The user must be informed that they entered the wrong type.
-                                System.out.println("The entered value must be an integer.");
-                            }
                         }
-                    }
-                    else {
-                        // The user needs to know if the binary search tree does not exist
-                        System.out.println("You must create the binary search tree first.");
-                    }
-                }
-
-                if (choice == 4) {
-                    // The array myList needs to be empty before we put anything into it.
-                    myList.clear();
-                    if (binaryTree.root != null) {
-                        // The PrintInOrder method is called and myList is updated and returned.
-                        binaryTree.printInOrder(binaryTree.root, myList);
-                        System.out.println("Nodes printed in order: " + myList);
-                    }
-                    else {
-                        System.out.println("You must create the binary search tree first.");
-                    }
-                }
-
-                if (choice == 5) {
-                    myList.clear();
-                    if (binaryTree.root != null) {
-                        // The printPreOrder method is called and myList is updated and returned.
-                        binaryTree.printPreOrder(binaryTree.root, myList);
-                        System.out.println( "Nodes printed in preorder: " + myList);
-                    }
-                    else {
-                        System.out.println("You must create the binary search tree first.");
-                    }
-                }
-
-                if (choice == 6) {
-                    myList.clear();
-
-                    if (binaryTree.root != null) {
-                        // The printPostOrder method is called and myList is updated and returned.
-                        binaryTree.printPostOrder(binaryTree.root, myList);
-
-                        System.out.println("Nodes printed in post order: " + myList);
-
+                        // Print the BST Inorder
+                        if (choice == 4) {
+                            // The PrintInOrder method is called, and myList is updated and returned.
+                            binaryTree.printInOrder(binaryTree.root, myList);
+                            System.out.println("Nodes printed in order: " + myList);
+                        }
+                        // Print the BST in Preorder
+                        if (choice == 5) {
+                            // The printPreOrder method is called, and myList is updated and returned.
+                            binaryTree.printPreOrder(binaryTree.root, myList);
+                            System.out.println( "Nodes printed in preorder: " + myList);
+                        }
+                        // Print the BST in Postorder
+                        if (choice == 6) {
+                            // The printPostOrder method is called, and myList is updated and returned.
+                            binaryTree.printPostOrder(binaryTree.root, myList);
+                            System.out.println("Nodes printed in post order: " + myList);
+                        }
                     }
                     else {
                         System.out.println("You must create the binary search tree first.");
@@ -161,17 +109,17 @@ public class Main {
                     System.exit(0);
                 }
 
-                // Handle invalid menu selection
+                // Handle invalid menu selections
                 if (choice > 7 || choice < 1) {
                     System.out.println("Please only enter a number from 1 through 7.");
                 }
-                // Handle any characters entered other than whole numbers
+
+              // Handle any characters entered other than whole numbers
             } catch (Exception e) {
                 System.out.println("Only whole numbers are accepted for menu selection.");
             }
         }
     }
-
 }
 
 
